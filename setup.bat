c0ccb295d8237accb10ba2405621ea2e4b9e08ef
krgna <122751473+krgna@users.noreply.github.com>
2023-02-15 15:40:28 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,13 +1,13 @@
 @echo off
 
-REM Check if pip is installed
+
 pip --version >nul 2>&1
 if %errorlevel% neq 0 (
   echo Error: pip is not installed.
   exit /b 1
 )
 
-REM Check if SpeechRecognition is installed
+
 pip show SpeechRecognition >nul 2>&1
 if %errorlevel% neq 0 (
   echo SpeechRecognition is not installed. Installing now...
